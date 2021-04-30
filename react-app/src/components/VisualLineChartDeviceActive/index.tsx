f1c98d7266abd4f987c3a0b1cf3151f3616f3b6b
MrTager <mrtager@qq.com>
2021-04-30 15:24:28 +0800

    update

--- react-app/src/components/VisualLineChartDeviceActive/index.tsx
+++ react-app/src/components/VisualLineChartDeviceActive/index.tsx
@@ -1,32 +1,101 @@
-import React, { PureComponent } from 'react';
-import ReactEcharts from 'echarts-for-react';
+// import ReactEcharts from 'echarts-for-react';
+import React,{useEffect} from "react"
+import * as echarts from 'echarts';
 import styles from './index.module.scss'
+import {
+    BarChart,
+    // 系列类型的定义后缀都为 SeriesOption
+    BarSeriesOption,
+    LineChart,
+    LineSeriesOption
+} from 'echarts/charts';
+import {
+    TitleComponent,
+    // 组件类型的定义后缀都为 ComponentOption
+    TitleComponentOption,
+    GridComponent,
+    GridComponentOption
+} from 'echarts/components';
+import {
+    CanvasRenderer
+} from 'echarts/renderers';
 
-const VisualLineChartDeviceActive = ()=>{
-    const getOption = ()=>{
-        let option = {
-            xAxis: {
-                type: 'category',
-                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-            },
-            yAxis: {
-                type: 'value'
-            },
-            series: [{
-                data: [820, 932, 901, 934, 1290, 1330, 1320],
-                type: 'line',
-                smooth: true
-            }]
-        };
-        return option
+type ECOption = echarts.ComposeOption< BarSeriesOption | LineSeriesOption | TitleComponentOption | GridComponentOption >;
+const VisualLineChartDeviceActive:React.FC = () => {
+    // const getOption = () => {
+    //     let option = {
+    //         xAxis: {
+    //             type: 'category',
+    //             data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    //         },
+    //         yAxis: {
+    //             type: 'value'
+    //         },
+    //         series: [{
+    //             data: [820, 932, 901, 934, 1290, 1330, 1320],
+    //             type: 'line',
+    //             smooth: true
+    //         }],
+    //     };
+    //     return option
+    // }
+    let myRef:any = null;
+    const setMyRef = (e:any) => {
+        myRef = e
     }
+    let myEchart = null;
+    useEffect(()=>{
+        myEchart = echarts.init(myRef);
+        myEchart.setOption({
+            legend: {
+                top: 'bottom'
+            },
+            toolbox: {
+                show: true,
+                feature: {
+                    mark: {show: true},
+                    dataView: {show: true, readOnly: false},
+                    restore: {show: true},
+                    saveAsImage: {show: true}
+                }
+            },
+            series: [
+                {
+                    name: '面积模式',
+                    type: 'pie',
+                    radius: [50, 100],
+                    center: ['50%', '50%'],
+                    roseType: 'area',
+                    itemStyle: {
+                        borderRadius: 8
+                    },
+                    data: [
+                        {value: 40, name: 'rose 1'},
+                        {value: 38, name: 'rose 2'},
+                        {value: 32, name: 'rose 3'},
+                        {value: 30, name: 'rose 4'},
+                        {value: 28, name: 'rose 5'},
+                        {value: 26, name: 'rose 6'},
+                        {value: 22, name: 'rose 7'},
+                        {value: 18, name: 'rose 8'}
+                    ]
+                }
+            ]
+        })
+        console.log('获取节点',myRef)
+    },[])
+
+
     return (
-        <div className={styles.box}>
-        <ReactEcharts
-            className={styles.content}
-            style={{width:'100%',height:'100%',padding:'0px'}}
-            option={getOption()}
-        ></ReactEcharts>
+        // <div className={styles.box}>
+        //     <ReactEcharts
+        //         className={styles.content}
+        //         style={{width:'100%',height:'250px',padding:'0px'}}
+        //         option={getOption()}
+        //     ></ReactEcharts>
+        // </div>
+        <div  className={styles.box}>
+            <div className={styles.content} ref={setMyRef}></div>
         </div>
     )
 }
--- react-app/src/page/Visual/index.tsx
+++ react-app/src/page/Visual/index.tsx
@@ -1,17 +1,55 @@
 
-import { memo } from 'react'
+import { memo,useState } from 'react'
 import styles from  './index.module.scss'
 import { VisualLineChartDeviceActive } from '../../components/LazyComponent_Visual'
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
 /**
  * 可视化面板
  */
-const Visual = () => {
+const screenStyle:object = {
+    width:"100%",
+    height:"100%",
+    backgroundColor:"yellow",
+}
+const screenStyle_full:object = {
+    position:"fixed",
+    width:"100%",
+    height:"100%",
+    backgroundColor:"red"
+}
+interface FullScreenHandle {
+    active: boolean;
+    // Specifies if attached element is currently full screen.
+  
+    enter: () => Promise<void>;
+    // Requests this element to go full screen.
+  
+    exit: () => Promise<void>;
+    // Requests this element to exit full screen.
+  
+    node: React.MutableRefObject<HTMLDivElement | null>;
+    // The attached DOM node
+  }
+  interface FullScreenProps {
+    handle: FullScreenHandle;
+    // Handle that helps operate the full screen state.
+  
+    onChange?: (state: boolean, handle: FullScreenHandle) => void;
+    // Optional callback that gets called when this screen changes state.
+    
+    className?: string;
+    // Optional prop allowing you to apply a custom class name to the FullScreen container
+  }
+const Visual:React.FC = () => {
+    const handle = useFullScreenHandle();
+    const handleToggle = handle.enter;
     return (
-        <div className={styles.visualPanel}>
-            <button>全屏显示</button>
+        <div className={styles.visualPanel} >
+            <button onClick={()=>handleToggle()}>全屏显示</button>
+            <FullScreen handle={handle}>
             <div className={styles.panelContent}>
-                <header>可视化面板</header>
+                <header></header>
                 <div className={styles.body}>
                     <div className={styles.side}>
                         <div className={styles.sideBox}>
@@ -22,6 +60,7 @@ const Visual = () => {
                     <div className={styles.side}>右</div>
                 </div>
             </div>
+            </FullScreen>
         </div>
     )
 }
